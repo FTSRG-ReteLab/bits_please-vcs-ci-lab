d27e2391d4a6b66a3b434d8a474b66b40ed5b30e
Liliána Bohus <boli96@gmail.com>
2018-02-19 09:13:30 -0800

    tachograph correction

--- train-tachograph/src/main/java/hu/bme/mit/train/tachograph/Tachograph.java
+++ train-tachograph/src/main/java/hu/bme/mit/train/tachograph/Tachograph.java
@@ -1,11 +1,10 @@
 package hu.bme.mit.train.tachograph;
 
 import com.google.common.collect.Table;
-import com.google.common.collect.ArrayTable;
 import com.google.common.collect.HashBasedTable;
 
 public class Tachograph {
-	Table<Integer, Integer, Integer> coll=ArrayTable.create(null);
+	Table<Integer, Integer, Integer> coll=HashBasedTable.create();
 	
 	public void record(long time, int pos, int speed){
 		coll.put((int)time, pos, speed);
