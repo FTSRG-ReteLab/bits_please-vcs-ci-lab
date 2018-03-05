e94a12fecab9d2035e68c705853ca8afa200f19a
szaboda <szdani2009@gmail.com>
2018-03-05 06:45:08 -0800

    speed fix

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -2,6 +2,7 @@ package hu.bme.mit.train.controller;
 
 import java.util.Calendar;
 import java.util.Timer;
+import java.util.TimerTask;
 
 import hu.bme.mit.train.interfaces.TrainController;
 import hu.bme.mit.train.tachograph.Tachograph;
